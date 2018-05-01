e0160398245e7169b56b55c473b55b82821ea7ca
Harald Fuchs <hari.fuchs@gmail.com>
2018-05-01 17:25:15 +0200

    Initial version

---
+++ T4/src/main/java/com/sap/test/hf/CustomerDAO.java
@@ -0,0 +1,30 @@
+package com.sap.test.hf;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+public class CustomerDAO {
+  @PersistenceContext
+  EntityManager em;
+
+  public CustomerDAO(EntityManager em) {
+    this.em = em;
+  }
+
+  public Optional<Customer> findById(long id) throws Exception {
+    return Optional.ofNullable(em.find(Customer.class, id));
+  }
+
+  public List<Customer> findAll() throws Exception {
+    TypedQuery<Customer> query = em.createQuery("select * from CUSTOMER", Customer.class);
+    return query.getResultList();
+  }
+
+  public Customer update(Customer customer) throws Exception {
+    return em.merge(customer);
+  }
+}
